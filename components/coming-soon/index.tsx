bcb762934e0aa066eee358fa27d949ebf2a00ca0
git-marcopitra <marcomorete73@hotmail.com>
2022-03-10 21:40:17 +0100

    :sparkles: feat: add lotties

--- components/coming-soon/index.tsx
+++ components/coming-soon/index.tsx
@@ -1,33 +1,26 @@
-import { useRouter } from 'next/router';
+import { Controls, Player } from '@lottiefiles/react-lottie-player';
 import { FC } from 'react';
 
-import { Routes, RoutesEnum } from '../../constants/routes';
-import { Box, Typography } from '../../elements';
-import ModalCard from '../modal-card';
-import { DoorSVG } from '../svg';
+import { Box } from '../../elements';
 
-const ComingSoon: FC = () => {
-  const { push } = useRouter();
-
-  const handleClose = () => push(Routes[RoutesEnum.Home]);
-
-  return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      backgroundImage="linear-gradient(226.51deg, #D5E0FF 34.24%, #D5E0FF 34.24%, #E7EDFF 45.58%, #EBF0FF 76.14%);"
+const ComingSoon: FC = () => (
+  <Box
+    width="100vw"
+    height="100vh"
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+    backgroundImage="linear-gradient(226.51deg, #D5E0FF 34.24%, #D5E0FF 34.24%, #E7EDFF 45.58%, #EBF0FF 76.14%);"
+  >
+    <Player
+      autoplay
+      loop
+      src="https://assets3.lottiefiles.com/packages/lf20_ypjdfhjl.json"
+      style={{ height: '300px', width: '300px' }}
     >
-      <ModalCard onClose={handleClose}>
-        <DoorSVG width="3.5rem" />
-        <Typography variant="normal" fontSize="XXL" mt="L">
-          Coming Soon
-        </Typography>
-      </ModalCard>
-    </Box>
-  );
-};
+      <Controls visible={false} />
+    </Player>
+  </Box>
+);
 
 export default ComingSoon;
