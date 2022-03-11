f8a113a73547098831dcc2a452db75738de0d7fb
git-marcopitra <marcomorete73@hotmail.com>
2022-03-11 09:24:56 +0100

    :bug: fix: recover the next button

--- views/home/components/road-map/road-map-item.tsx
+++ views/home/components/road-map/road-map-item.tsx
@@ -96,7 +96,7 @@ const RoadMapItem: FC<RoadMapItemProps> = ({
           <Button variant="neutral" onClick={onBack} disabled={step === 0}>
             &larr;
           </Button>
-          {position === length - 1 && (
+          {position !== length - 1 && (
             <Button variant="tertiary" onClick={onNext}>
               Next
             </Button>
