b321c248721f45d85ec8e44ed8d2725a44fd460e
josemvcerqueira <jose@ilovemochi.com>
2022-04-03 00:57:07 +0100

    :wrench: fix: remove unused enum

--- constants/index.ts
+++ constants/index.ts
@@ -5,11 +5,3 @@ export const NetworkContextName = 'NETWORK';
 export const SECONDS_IN_A_YEAR = BigNumber.from(3.154e7);
 
 export const ZERO = BigNumber.from(0);
-
-export enum Rounding {
-  Nothing,
-  UP,
-  HalfUp,
-  HalfDown,
-  Down,
-}
