700b0ff0c4c531502e33898d3c9029799e85ad4e
git-marcopitra <marcomorete73@hotmail.com>
2022-04-03 20:30:52 +0100

    :bug: fix: mobile ui dropdown and svgs broken

--- elements/dropdown/dropdown.types.ts
+++ elements/dropdown/dropdown.types.ts
@@ -24,6 +24,7 @@ export interface DropdownProps {
   suffix?: ReactNode;
   header?: ReactNode;
   footer?: ReactNode;
+  relative?: boolean;
   buttonMode?: boolean;
   customItems?: boolean;
   customTitle?: boolean;
