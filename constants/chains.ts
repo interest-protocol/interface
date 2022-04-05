63f4dd52cf3bb1154fde813cab4fcf1e21dbf789
josemvcerqueira <jose@ilovemochi.com>
2022-04-05 11:03:04 +0100

    :package: chore: add vercel cli

--- constants/chains.ts
+++ constants/chains.ts
@@ -46,8 +46,9 @@ export const CHAINS = {
     chainName: 'Binance Smart Chain Test Net',
     nativeCurrency: BNB,
     rpcUrls: [
+      process.env.NEXT_PUBLIC_BSC_TEST_NET_JSON_RPC ||
+        'https://data-seed-prebsc-2-s1.binance.org:8545/',
       'https://data-seed-prebsc-1-s1.binance.org:8545/',
-      'https://data-seed-prebsc-2-s1.binance.org:8545/',
       'https://data-seed-prebsc-1-s2.binance.org:8545/',
       'https://data-seed-prebsc-2-s2.binance.org:8545/',
       'https://data-seed-prebsc-1-s3.binance.org:8545/',
