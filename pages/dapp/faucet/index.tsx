9ce71d3760347d7fc4c01c534564540f80e11d0a
git-marcopitra <marcomorete73@hotmail.com>
2022-06-04 21:57:00 +0100

    :bug: fix: remove unncessary imports

--- pages/dapp/faucet/index.tsx
+++ pages/dapp/faucet/index.tsx
@@ -1,7 +1,5 @@
 import { NextPage } from 'next';
 
-import { Web3Manager } from '@/components';
-import { CHAIN_ID } from '@/sdk';
 import Faucet from '@/views/dapp/views/faucet';
 
 const FaucetPage: NextPage = () => <Faucet />;
