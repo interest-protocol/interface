e02d607d93d77c2eff5d9c7cf13d6ec1c3feceb3
josemvcerqueira <jose@ilovemochi.com>
2022-06-27 11:02:03 +0100

    :white_check_mark: test: big number utils

--- utils/big-number/__tests__/add-positive-number-strings.spec.ts
+++ utils/big-number/__tests__/add-positive-number-strings.spec.ts
@@ -1,4 +1,4 @@
-import { addPositiveNumberStrings } from '../index';
+import { addPositiveNumberStrings } from '..';
 
 describe(addPositiveNumberStrings.name, () => {
   it('should be 44 with 18 zeros from sum of 11 and 33', () => {
@@ -7,5 +7,29 @@ describe(addPositiveNumberStrings.name, () => {
       '33000000000000000000'
     );
     expect(positiveNumber.toString()).toEqual('44000000000000000000');
+  });
+
+  it('returns 0 if x and y are NaNs', () => {
+    expect(addPositiveNumberStrings('world', '33000000000000000000')).toEqual(
+      '0'
+    );
+    expect(addPositiveNumberStrings('33000000000000000000', 'hello')).toEqual(
+      '0'
+    );
+  });
+
+  it('returns 0 if x and y are lower than 0', () => {
+    expect(
+      addPositiveNumberStrings(
+        (-Math.random()).toString(),
+        '33000000000000000000'
+      )
+    ).toEqual('0');
+    expect(
+      addPositiveNumberStrings(
+        '33000000000000000000',
+        (-Math.random()).toString()
+      )
+    ).toEqual('0');
   });
 });
--- utils/big-number/__tests__/from-positive-number.spec.ts
+++ utils/big-number/__tests__/from-positive-number.spec.ts
@@ -1,8 +1,31 @@
-import { fromPositiveNumber } from '../index';
+import { MAX_NUMBER_INPUT_VALUE, ZERO_BIG_NUMBER } from '@/sdk';
+
+import { fromPositiveNumber } from '..';
 
 describe(fromPositiveNumber.name, () => {
   it('should be have a 18 zeros', () => {
     const positiveNumber = fromPositiveNumber(18)(10);
     expect(positiveNumber.toString()).toEqual('10000000000000000000');
+  });
+
+  it(`returns 0 if X is larger than ${MAX_NUMBER_INPUT_VALUE}`, () => {
+    expect(fromPositiveNumber(MAX_NUMBER_INPUT_VALUE, 1)).toEqual(
+      ZERO_BIG_NUMBER
+    );
+    expect(fromPositiveNumber(MAX_NUMBER_INPUT_VALUE + 1, 1)).toEqual(
+      ZERO_BIG_NUMBER
+    );
+  });
+
+  it('returns 0 if x is negative', () => {
+    expect(fromPositiveNumber(-Math.random(), 1)).toEqual(ZERO_BIG_NUMBER);
+  });
+
+  it('returns 0 if y is negative', () => {
+    expect(fromPositiveNumber(100, -Math.random())).toEqual(ZERO_BIG_NUMBER);
+  });
+
+  it('returns 0 if y is higher than 50', () => {
+    expect(fromPositiveNumber(100, 51)).toEqual(ZERO_BIG_NUMBER);
   });
 });
