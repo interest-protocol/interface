d47c4efdefaac1e9fc5c894baee00a966f89c0d6
git-marcopitra <marcomorete73@hotmail.com>
2022-07-04 23:04:19 +0100

    :green_heart: fix: build bug

--- views/home/components/index.ts
+++ views/home/components/index.ts
@@ -3,5 +3,3 @@ export { default as DineroMarkets } from './dinero-markets';
 export { default as Earn } from './earn';
 export { default as Hero } from './hero';
 export { default as MailMarkets } from './mail-markets';
-export { default as RoadMap } from './road-map';
-export { default as Subscribe } from './subscribe';
