2c81362a52a3c89ff649c4fa3d7ab18abb14675f
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2022-07-17 15:28:29 +0100

    💄 responsiveness adjusts (#64)

--- views/home/components/earn/index.tsx
+++ views/home/components/earn/index.tsx
@@ -21,13 +21,12 @@ const Earn: FC = () => {
 
   return (
     <Box
-      pt="10rem"
-      mt="-10rem"
       as="section"
       position="relative"
+      mt={['-4rem', 'L', 'XL', 'NONE']}
       backgroundImage="linear-gradient(180deg, rgba(248, 248, 248, 0) 0%, #F0F0F0 33.83%)"
     >
-      <Container bg={['#EEE', '#EEE', '#EEE', 'transparent']}>
+      <Container>
         <Box
           display="flex"
           alignItems="center"
