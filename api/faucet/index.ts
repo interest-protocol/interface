d76a7dcaac050127488f51d993f9c2cb24d0eba6
KipandaJr <antoniokipandacardoso@gmail.com>
2022-08-03 17:46:14 +0100

    :recycle: fix: refactor api/faucet and delete unused package

--- api/faucet/index.ts
+++ api/faucet/index.ts
@@ -3,7 +3,7 @@ import { ethers } from 'ethers';
 import MintABI from '@/sdk/abi/mint.abi.json';
 import MintOldABI from '@/sdk/abi/mint-old.abi.json';
 
-import { MintAbi } from '../../types/ethers-contracts';
+import { MintAbi, MintOldAbi } from '../../types/ethers-contracts';
 import { ERC20Mint, ERC20MintOld } from './faucet.types';
 
 export const erc20Mint: ERC20Mint = (signer, token, account, amount) => {
@@ -13,7 +13,7 @@ export const erc20Mint: ERC20Mint = (signer, token, account, amount) => {
 };
 
 export const erc20MintOld: ERC20MintOld = (signer, token, amount) => {
-  const contract = new ethers.Contract(token, MintOldABI, signer);
+  const contract = new ethers.Contract(token, MintOldABI, signer) as MintOldAbi;
 
   return contract.mint(amount);
 };
