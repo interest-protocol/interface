5f22b469f5375e80f4d6413fe9b4c31294b333f1
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2022-08-20 20:00:47 +0100

    :lipstick: fix: background and DOM error (#115)
    Co-authored-by: Jose Cerqueira <jose@interestprotocol.com>

--- views/dapp/views/earn/components/earn-table/index.tsx
+++ views/dapp/views/earn/components/earn-table/index.tsx
@@ -188,7 +188,7 @@ const EarnTable: FC<EarnTableProps> = ({
           <Table
             hasButton
             backgroundColorMap={filteredFarms.map((farm) => ({
-              bg: farm.isLive ? 'unset' : 'bottomBackground',
+              bg: farm.isLive ? 'foreground' : 'bottomBackground',
             }))}
             headings={[
               {
