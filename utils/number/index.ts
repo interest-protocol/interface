9916e4cb2f5fc54e88460133c3cbfdfa1c63767b
git-marcopitra <marcomorete73@hotmail.com>
2022-09-02 15:42:43 +0100

    :bug: fix: locale string to utils fn

--- utils/number/index.ts
+++ utils/number/index.ts
@@ -1,5 +1,20 @@
+import { Fraction, Rounding } from '@/sdk';
+
 export const toFixedToPrecision = (
   x: string | number,
   fixedArg = 2,
   precisionArg = 2
-) => (+(+x).toFixed(fixedArg)).toPrecision(precisionArg);
+): string => (+(+x).toFixed(fixedArg)).toPrecision(precisionArg);
+
+export const numberToString = (
+  x: number,
+  significant: 8,
+  rounding: Rounding.ROUND_HALF_UP
+): string =>
+  Fraction.from(x).toSignificant(
+    significant,
+    {
+      decimalSeparator: '',
+    },
+    rounding
+  );
