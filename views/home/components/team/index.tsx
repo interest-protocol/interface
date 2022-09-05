d69613c1b3ecda0ce2e828b0317d19f6c2dfc493
git-marcopitra <marcomorete73@hotmail.com>
2022-09-05 16:29:08 +0100

    :sparkles: feat: add team seaction

--- views/home/components/team/index.tsx
+++ views/home/components/team/index.tsx
@@ -1,11 +1,13 @@
-import { always } from 'ramda';
+import { always, toPairs } from 'ramda';
 import { FC } from 'react';
 import { v4 } from 'uuid';
 
 import { Container } from '@/components';
 import { Box, Typography } from '@/elements';
+import { LogoSVG } from '@/svg';
 
-import { TEAM_MEMBERS } from './team.data';
+import { FlipMemberCard } from './team.animation';
+import { SOCIAL_SVG, TEAM_MEMBERS } from './team.data';
 
 const Team: FC = always(
   <Box bg="foreground">
@@ -34,24 +36,111 @@ const Team: FC = always(
       </Typography>
       <Box
         pt="XL"
-        mx={['L', 'XXXL']}
         display="grid"
+        mx={['L', 'XXXL']}
         gridRowGap={['2rem', '5rem']}
-        gridColumnGap={['3rem', '7rem']}
-        gridTemplateColumns={['1fr 1fr', '1fr 1fr', '1fr 1fr 1fr']}
+        gridColumnGap={['3rem', '3rem', '3rem', '7rem']}
+        gridTemplateColumns={[
+          '1fr',
+          '1fr 1fr',
+          '1fr 1fr',
+          '1fr 1fr',
+          '1fr 1fr 1fr',
+        ]}
       >
-        {TEAM_MEMBERS.map(({ name, role }) => (
-          <Box key={v4()}>
-            <img
-              alt="cube"
-              width="100%"
-              loading="lazy"
-              decoding="async"
-              src="https://thispersondoesnotexist.com/image"
-            />
-
-            <Typography variant="normal">{name}</Typography>
+        {TEAM_MEMBERS.map(({ name, role, social, image, bio }) => (
+          <Box key={v4()} as="article">
+            <FlipMemberCard height={['25rem', '25rem', '25rem', '30rem']}>
+              <Box
+                width="100%"
+                height="100%"
+                position="relative"
+                className="flipWrapper"
+              >
+                <Box
+                  mb="L"
+                  height="100%"
+                  overflow="hidden"
+                  position="relative"
+                  className="flipImage"
+                >
+                  <img
+                    alt={name}
+                    src={image}
+                    width="100%"
+                    height="100%"
+                    loading="lazy"
+                    decoding="async"
+                    style={{
+                      objectFit: 'cover',
+                    }}
+                  />
+                  <Box
+                    p="L"
+                    pt="XXL"
+                    pl="XXL"
+                    right="0"
+                    bottom="0"
+                    width="8rem"
+                    position="absolute"
+                    backgroundImage="linear-gradient(135deg, #0000 45%, #000A)"
+                  >
+                    <LogoSVG width="100%" />
+                  </Box>
+                </Box>
+                <Box
+                  p="XL"
+                  height="100%"
+                  display="flex"
+                  bg="foreground"
+                  overflow="hidden"
+                  alignItems="center"
+                  className="flipBio"
+                  flexDirection="column"
+                  border="0.15rem solid"
+                  borderColor="outline"
+                  borderBottom="0.5rem solid"
+                  borderBottomColor="accent"
+                >
+                  <Box width="4rem">
+                    <LogoSVG width="100%" />
+                  </Box>
+                  <Typography variant="normal" mt="L" lineHeight="1.6">
+                    {bio}
+                  </Typography>
+                </Box>
+              </Box>
+            </FlipMemberCard>
+            <Typography
+              as="h3"
+              variant="normal"
+              fontWeight="bold"
+              letterSpacing="1.5px"
+              textTransform="uppercase"
+            >
+              {name}
+            </Typography>
             <Typography variant="normal">{role}</Typography>
+            <Box mt="M">
+              {toPairs(social).map(([network, link]) => {
+                const Icon = SOCIAL_SVG[network];
+                return (
+                  <a href={link} target="_blank" rel="noreferrer" key={v4()}>
+                    <Box
+                      mx="M"
+                      as="span"
+                      width="1.6rem"
+                      display="inline-block"
+                      hover={{
+                        color: 'accent',
+                      }}
+                    >
+                      <Icon width="100%" />
+                    </Box>
+                  </a>
+                );
+              })}
+            </Box>
           </Box>
         ))}
       </Box>
--- views/home/components/team/team.data.ts
+++ views/home/components/team/team.data.ts
@@ -1,18 +1,66 @@
+import { GithubSVG, LinkedinSVG, TwitterSVG } from '@/svg';
+
+export const SOCIAL_SVG = {
+  github: GithubSVG,
+  linkedin: LinkedinSVG,
+  twitter: TwitterSVG,
+};
+
 export const TEAM_MEMBERS = [
   {
     name: 'José Cerqueira',
     role: 'Co-Founder & CEO',
+    image: 'https://thispersondoesnotexist.com/image?v=10',
+    social: {
+      github: 'https://github.com/josemvcerqueira',
+      linkedin: 'https://www.linkedin.com/in/josemvcerqueira/',
+      twitter: 'https://twitter.com/josemvcerqueira',
+    },
+    bio: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Itaque aspernatur ducimus, vitae molestias libero quia quasi
+    adipisci minima sunt odit voluptatibus quae, veniam maxime
+    ex ipsum amet, natus optio illo.`,
   },
   {
     name: 'Pedro Nelumba',
     role: 'Co-Founder & CFO',
+    image: 'https://thispersondoesnotexist.com/image?v=20',
+    social: {
+      github: 'https://github.com/josemvcerqueira',
+      linkedin: 'https://www.linkedin.com/in/josemvcerqueira/',
+      twitter: 'https://twitter.com/josemvcerqueira',
+    },
+    bio: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Itaque aspernatur ducimus, vitae molestias libero quia quasi
+    adipisci minima sunt odit voluptatibus quae, veniam maxime
+    ex ipsum amet, natus optio illo.`,
   },
   {
     name: 'Marco Pitra',
     role: 'Frontend Developer',
+    image: 'https://thispersondoesnotexist.com/image?v=30',
+    social: {
+      github: 'https://github.com/josemvcerqueira',
+      linkedin: 'https://www.linkedin.com/in/josemvcerqueira/',
+      twitter: 'https://twitter.com/josemvcerqueira',
+    },
+    bio: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Itaque aspernatur ducimus, vitae molestias libero quia quasi
+    adipisci minima sunt odit voluptatibus quae, veniam maxime
+    ex ipsum amet, natus optio illo.`,
   },
   {
     name: 'António Kipanda',
     role: 'Frontend Developer',
+    image: 'https://thispersondoesnotexist.com/image?v=40',
+    social: {
+      github: 'https://github.com/josemvcerqueira',
+      linkedin: 'https://www.linkedin.com/in/josemvcerqueira/',
+      twitter: 'https://twitter.com/josemvcerqueira',
+    },
+    bio: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Itaque aspernatur ducimus, vitae molestias libero quia quasi
+    adipisci minima sunt odit voluptatibus quae, veniam maxime
+    ex ipsum amet, natus optio illo.`,
   },
 ];
