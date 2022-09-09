91a7635f7716620bb06b96396e0420671e6cd821
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2022-09-09 12:47:13 +0100

    :sparkles: feat: add dex disable auto fetch prices (#128)

--- views/dapp/views/dex/swap/swap.types.ts
+++ views/dapp/views/dex/swap/swap.types.ts
@@ -14,6 +14,7 @@ export interface ISwapForm {
 export interface LocalSwapSettings {
   slippage: string; // 20 equals 20%
   deadline: number; // minutes
+  autoFetch: boolean; // minutes
 }
 
 export interface AmountCacheValue {
