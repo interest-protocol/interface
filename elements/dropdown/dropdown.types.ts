351f4e6f7d89e44183e27786b24fc98242684f87
git-marcopitra <marcomorete73@hotmail.com>
2022-10-23 20:01:50 +0100

    :bug: fix: dropdown position

--- elements/dropdown/dropdown.types.ts
+++ elements/dropdown/dropdown.types.ts
@@ -28,7 +28,6 @@ export interface DropdownProps {
   suffix?: ReactNode;
   header?: ReactNode;
   footer?: ReactNode;
-  relative?: boolean;
   bgSelected?: string;
   fromRight?: boolean;
   buttonMode?: boolean;
