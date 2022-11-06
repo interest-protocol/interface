79bcec8317fd199729940657166a1432707b2d6e
git-marcopitra <marcomorete73@hotmail.com>
2022-11-06 03:48:12 +0100

    :construction: refactor: remove styled-system

--- stylin/render-pseudo-selector.ts
+++ stylin/render-pseudo-selector.ts
@@ -3,10 +3,14 @@ import { CSSInterpolation } from '@emotion/serialize';
 import renderResponsiveStyle from './render-responsive-style';
 import { TRenderPseudoSelector, TStyleKeys } from './stylin.types';
 
-const renderPseudoSelector: TRenderPseudoSelector = (selector, styles) => ({
+const renderPseudoSelector: TRenderPseudoSelector = (
+  theme,
+  selector,
+  styles
+) => ({
   [`:${selector}`]: Object.entries(styles).reduce(
     (acc, [prop, value]) =>
-      acc.concat(renderResponsiveStyle(prop as TStyleKeys, value)),
+      acc.concat(renderResponsiveStyle(theme, prop as TStyleKeys, value)),
     [] as Array<CSSInterpolation>
   ),
 });
