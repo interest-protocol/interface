e268039cf01f49e52345ad038011eaacb1deb3a0
Jose Cerqueira <jose@interestprotocol.com>
2023-02-27 06:00:24 +0000

    :bug: fix: jest config

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom';
 import ReactModal from 'react-modal';
+import { jest } from '@jest/globals';
 
 jest.spyOn(ReactModal, 'setAppElement').mockImplementation(() => {});
