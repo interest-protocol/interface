b6becdd85578a4dd9bba41c9db01be7d7cde5776
José Cerqueira <josecerqueira@protonmail.com>
2023-03-08 10:18:46 +0000

    fix: fetch
    Co-authored-by: Jose Cerqueira <jose@interestprotocol.com>

--- api/analytics.ts
+++ api/analytics.ts
@@ -4,15 +4,10 @@ export const incrementTX = async (address: string) => {
     'https://ipx-analytics-server.vercel.app/api/v1/analytics/incrementTX',
     {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
+      credentials: 'include', // include, *same-origin, omit
       headers: {
         'Content-Type': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify({ address }), // body data type must match "Content-Type" header
     }
   );
