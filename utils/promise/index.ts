c65b37a8ffc9476e98a4510d80f88275583cd285
José Cerqueira <josecerqueira@protonmail.com>
2023-03-11 17:00:59 +0000

    fix: remove sleep function (#107)

--- utils/promise/index.ts
+++ utils/promise/index.ts
@@ -17,5 +17,3 @@ export const noop = () => {};
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export const asyncNoop = async () => {};
-
-export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
