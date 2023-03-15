2ddeeb0e441a50b4ef134e847fa08884291f4484
KipandaJr <antoniokipandacardoso@gmail.com>
2023-03-15 06:35:23 +0100

    :sparkles: feat: adding media kit in footer

--- views/home/components/subscribe/index.tsx
+++ views/home/components/subscribe/index.tsx
@@ -3,8 +3,10 @@ import { useTranslations } from 'next-intl';
 import { propOr } from 'ramda';
 import { FC, useState } from 'react';
 import toast from 'react-hot-toast';
+import { v4 } from 'uuid';
 
-import { Container } from '@/components';
+import { Container, SocialMediaCard } from '@/components';
+import { SOCIAL_MEDIAS } from '@/constants';
 import { LoadingSVG } from '@/svg';
 import { capitalize } from '@/utils';
 import { logGenericEvent } from '@/utils/analytics';
@@ -49,85 +51,120 @@ const Subscribe: FC = () => {
 
   return (
     <Box bg="text">
-      <Container
-        py="XXXL"
-        as="section"
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        flexDirection={['column', 'column', 'column', 'row']}
-      >
-        <Typography
-          mt="M"
-          as="h2"
-          variant="title1"
-          maxWidth="40rem"
-          color="textInverted"
-          textTransform="capitalize"
-          fontSize={['1.9rem', '2.75rem']}
-          textAlign={['center', 'center', 'center', 'left']}
-        >
-          {t('landingPage.subscribeSectionText')}
-        </Typography>
+      <Container as="section" pt="XXXL" pb={['3rem', '3rem', '3rem', 'XXL']}>
         <Box
-          as="form"
-          width="100%"
           display="flex"
-          alignItems="stretch"
-          // @ts-ignore
-          onSubmit={handleSubscribe}
-          mt={['XL', 'XL', 'XL', 'NONE']}
-          flexDirection={['column', 'row']}
-          justifyContent={['center', 'center', 'center', 'flex-end']}
+          alignItems="center"
+          justifyContent="space-between"
+          flexDirection={['column', 'column', 'column', 'row']}
         >
-          <Input
-            p="L"
-            type="email"
-            bg="outline"
-            border="none"
-            outline="none"
-            borderRadius="S"
-            mr={['NONE', 'S']}
+          <Typography
+            mt="M"
+            as="h2"
+            variant="title1"
+            maxWidth="40rem"
             color="textInverted"
-            width={['100%', '20rem']}
-            backgroundColor="#262626"
-            placeholder={t('landingPage.subscribeInputPlaceholder')}
-          />
-          <Button
-            p="L"
-            type="submit"
-            effect="hover"
-            display="flex"
-            disabled={loading}
-            variant="primary"
-            fontWeight="bold"
-            mt={['M', 'NONE']}
-            ml={['NONE', 'S']}
-            lineHeight="1.7rem"
-            letterSpacing={1.09}
-            justifyContent="center"
-            minWidth={['100%', '10rem']}
-            textTransform="uppercase"
-            alignItems="center"
+            textTransform="capitalize"
+            fontSize={['1.9rem', '2.75rem']}
+            textAlign={['center', 'center', 'center', 'left']}
           >
-            {t('landingPage.subscribeButton', { isLoading: loading ? 1 : 0 })}
-            {loading && (
-              <Box
-                ml="L"
-                display="inline-flex"
-                position="relative"
-                height="1rem"
-                width="1rem"
+            {t('landingPage.subscribeSectionText')}
+          </Typography>
+          <Box>
+            <Box
+              as="form"
+              width="100%"
+              display="flex"
+              alignItems="stretch"
+              // @ts-ignore
+              onSubmit={handleSubscribe}
+              mt={['XL', 'XL', 'XL', 'NONE']}
+              flexDirection={['column', 'row']}
+              justifyContent={['center', 'center', 'center', 'flex-end']}
+            >
+              <Input
+                p="L"
+                type="email"
+                bg="outline"
+                border="none"
+                outline="none"
+                borderRadius="S"
+                mr={['NONE', 'S']}
+                color="textInverted"
+                width={['100%', '20rem']}
+                backgroundColor="#262626"
+                placeholder={t('landingPage.subscribeInputPlaceholder')}
+              />
+              <Button
+                p="L"
+                type="submit"
+                effect="hover"
+                display="flex"
+                disabled={loading}
+                variant="primary"
+                fontWeight="bold"
+                mt={['M', 'NONE']}
+                ml={['NONE', 'S']}
+                lineHeight="1.7rem"
+                letterSpacing={1.09}
+                justifyContent="center"
+                minWidth={['100%', '10rem']}
+                textTransform="uppercase"
+                alignItems="center"
               >
-                <LoadingSVG
-                  maxHeight="1rem"
-                  maxWidth="1rem"
-                  height="100%"
-                  width="100%"
-                />
+                {t('landingPage.subscribeButton', {
+                  isLoading: loading ? 1 : 0,
+                })}
+                {loading && (
+                  <Box
+                    ml="L"
+                    display="inline-flex"
+                    position="relative"
+                    height="1rem"
+                    width="1rem"
+                  >
+                    <LoadingSVG
+                      maxHeight="1rem"
+                      maxWidth="1rem"
+                      height="100%"
+                      width="100%"
+                    />
+                  </Box>
+                )}
+              </Button>
+            </Box>
+            <Box
+              as="nav"
+              display="flex"
+              flexDirection={['column', 'column', 'column', 'row']}
+              mt={['1.688rem', '1.688rem', '1.688rem', 'NONE']}
+              alignItems="center"
+              pt="M"
+            >
+              <Typography
+                variant="normal"
+                as="p"
+                fontWeight="400"
+                letterSpacing="1.5px"
+                color="textInverted"
+                textTransform="uppercase"
+                fontSize="0.7rem"
+              >
+                {t('landingPage.followUs')}
+              </Typography>
+              <Box
+                display="flex"
+                mb={['0.313rem', '0.313rem', '0.313rem', 'NONE']}
+                mt={['0.688rem', '0.688rem', '0.688rem', 'NONE']}
+                alignItems="center"
+                color="textInverted"
+              >
+                {SOCIAL_MEDIAS.map((socialMediaData) => (
+                  <SocialMediaCard {...socialMediaData} key={v4()} />
+                ))}
               </Box>
-            )}
-          </Button>
+            </Box>
+          </Box>
         </Box>
       </Container>
     </Box>
