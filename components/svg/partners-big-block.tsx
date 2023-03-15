8e5db96cabb92b03f268e96c0c7006841daa3aa8
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2023-03-15 10:33:38 +0100

    feat: partners section on LP

--- components/svg/partners-big-block.tsx
+++ components/svg/partners-big-block.tsx
@@ -2,45 +2,41 @@ import { FC } from 'react';
 
 import { SVGProps } from './svg.types';
 
-const IntegrationsSmallBlock: FC<SVGProps> = ({
-  maxHeight,
-  maxWidth,
-  ...props
-}) => (
+const PartnersBigBlock: FC<SVGProps> = ({ maxHeight, maxWidth, ...props }) => (
   <svg
     style={{ maxHeight, maxWidth }}
-    viewBox="0 0 126 185"
+    viewBox="0 0 339 392"
     fill="none"
     {...props}
   >
     <path
       opacity="0.1"
-      d="M62.6977 0L125.395 36.25V148.75L62.6977 185L0 148.75V36.25L62.6977 0Z"
-      fill="url(#paint0_linear_97_48)"
+      d="M169.5 0L339 98V294L169.5 392L0 294V98L169.5 0Z"
+      fill="url(#paint0_linear_612_7)"
     />
     <path
       opacity="0.1"
-      d="M62.6977 0L125.395 36.25V36.6199L62.6977 72.8699L0 36.6199V36.25L62.6977 0Z"
-      fill="url(#paint1_linear_97_48)"
+      d="M169.5 0L339 98V99L169.5 197L0 99V98L169.5 0Z"
+      fill="url(#paint1_linear_612_7)"
     />
     <defs>
       <linearGradient
-        id="paint0_linear_97_48"
-        x1="62.6977"
+        id="paint0_linear_612_7"
+        x1="169.5"
         y1="0"
-        x2="62.6977"
-        y2="145"
+        x2="169.5"
+        y2="392"
         gradientUnits="userSpaceOnUse"
       >
         <stop stopColor="#111111" />
         <stop offset="1" stopOpacity="0" />
       </linearGradient>
       <linearGradient
-        id="paint1_linear_97_48"
-        x1="62.6977"
+        id="paint1_linear_612_7"
+        x1="169.5"
         y1="0"
-        x2="62.6977"
-        y2="145"
+        x2="169.5"
+        y2="392"
         gradientUnits="userSpaceOnUse"
       >
         <stop stopColor="#111111" />
@@ -50,4 +46,4 @@ const IntegrationsSmallBlock: FC<SVGProps> = ({
   </svg>
 );
 
-export default IntegrationsSmallBlock;
+export default PartnersBigBlock;
