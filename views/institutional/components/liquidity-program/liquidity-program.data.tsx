008e056d06e7e591cb0d5a5d3c80a4bf6aa586f2
Okusindja de Almeida <91469357+okusindja@users.noreply.github.com>
2023-05-18 08:33:31 +0100

    🔥 remove center mode from liquidity program (#274)
    * chore: remove center mode from liquidity program
    
    * fix: ts
    
    ---------
    
    Co-authored-by: git-marcopitra <marcomorete73@hotmail.com>

--- views/institutional/components/liquidity-program/liquidity-program.data.tsx
+++ views/institutional/components/liquidity-program/liquidity-program.data.tsx
@@ -1,48 +1,29 @@
-const CaretLeft: FC = (props) => {
-  const { className, onClick } = props;
-  return (
-    <svg
-      className={className}
-      onClick={onClick}
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M20 26L10 16L20 6"
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { FC, SVGAttributes } from 'react';
 
-const CaretRight: FC = (props) => {
-  const { className, onClick } = props;
-  return (
-    <svg
-      className={className}
-      onClick={onClick}
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M12 6L22 16L12 26"
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-};
+const CaretLeft: FC<SVGAttributes<SVGSVGElement>> = (props) => (
+  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" {...props}>
+    <path
+      d="M20 26L10 16L20 6"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const CaretRight: FC<SVGAttributes<SVGSVGElement>> = (props) => (
+  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" {...props}>
+    <path
+      d="M12 6L22 16L12 26"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
 
 export const sliderSettings = {
   speed: 500,
@@ -63,7 +44,6 @@ export const sliderSettings = {
       breakpoint: 628,
       settings: {
         slidesToShow: 1,
-        centerMode: true,
       },
     },
   ],
