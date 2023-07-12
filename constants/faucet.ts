f19e4d99f484f9d6fed4d475ed29e4e950ca79b9
José Cerqueira <josecerqueira@protonmail.com>
2023-07-12 17:38:09 +0100

    fix: max borrow value (#396)

--- constants/faucet.ts
+++ constants/faucet.ts
@@ -3,125 +3,6 @@ import { TOKEN_SYMBOL } from 'lib';
 import { FC } from 'react';
 
 import { SVGProps } from '@/components/svg/svg.types';
-import {
-  BinanceSVG,
-  BitcoinSVG,
-  EtherSVG,
-  SuiSVG,
-  USDCoinSVG,
-  USDTSVG,
-} from '@/svg';
-
-export const FAUCET_TOKENS = {
-  [Network.DEVNET]: [
-    {
-      symbol: TOKEN_SYMBOL.SUI,
-      type: COIN_TYPE[Network.DEVNET].SUI,
-      Icon: SuiSVG,
-      name: 'SUI',
-      decimals: 9,
-    },
-    {
-      symbol: TOKEN_SYMBOL.BTC,
-      type: COIN_TYPE[Network.DEVNET].BTC,
-      Icon: BitcoinSVG,
-      name: 'Bitcoin',
-      decimals: 9,
-    },
-    {
-      symbol: TOKEN_SYMBOL.BNB,
-      type: COIN_TYPE[Network.DEVNET].BNB,
-      Icon: BinanceSVG,
-      name: 'Binance Coin',
-      decimals: 9,
-    },
-    {
-      symbol: TOKEN_SYMBOL.ETH,
-      type: COIN_TYPE[Network.DEVNET].ETH,
-      Icon: EtherSVG,
-      name: 'ETHER',
-      decimals: 9,
-    },
-    {
-      symbol: TOKEN_SYMBOL.USDC,
-      type: COIN_TYPE[Network.DEVNET].USDC,
-      Icon: USDCoinSVG,
-      name: 'USDC',
-      decimals: 9,
-    },
-    {
-      symbol: TOKEN_SYMBOL.USDT,
-      type: COIN_TYPE[Network.DEVNET].USDT,
-      Icon: USDTSVG,
-      name: 'USDT',
-      decimals: 9,
-    },
-  ] as ReadonlyArray<{
-    symbol: TOKEN_SYMBOL.SUI;
-    type: string;
-    Icon: FC<SVGProps>;
-    name: string;
-    decimals: number;
-  }>,
-  [Network.TESTNET]: [
-    {
-      symbol: TOKEN_SYMBOL.SUI,
-      type: COIN_TYPE[Network.TESTNET].SUI,
-      Icon: SuiSVG,
-      name: 'SUI',
-      decimals: 9,
-    },
-    {
-      symbol: TOKEN_SYMBOL.BTC,
-      type: COIN_TYPE[Network.TESTNET].BTC,
-      Icon: BitcoinSVG,
-      name: 'Bitcoin',
-      decimals: 9,
-    },
-    {
-      symbol: TOKEN_SYMBOL.BNB,
-      type: COIN_TYPE[Network.TESTNET].BNB,
-      Icon: BinanceSVG,
-      name: 'Binance Coin',
-      decimals: 9,
-    },
-    {
-      symbol: TOKEN_SYMBOL.ETH,
-      type: COIN_TYPE[Network.TESTNET].ETH,
-      Icon: EtherSVG,
-      name: 'ETHER',
-      decimals: 9,
-    },
-    {
-      symbol: TOKEN_SYMBOL.USDC,
-      type: COIN_TYPE[Network.TESTNET].USDC,
-      Icon: USDCoinSVG,
-      name: 'USDC',
-      decimals: 9,
-    },
-    {
-      symbol: TOKEN_SYMBOL.USDT,
-      type: COIN_TYPE[Network.TESTNET].USDT,
-      Icon: USDTSVG,
-      name: 'USDT',
-      decimals: 9,
-    },
-  ] as ReadonlyArray<{
-    symbol: TOKEN_SYMBOL.SUI;
-    type: string;
-    Icon: FC<SVGProps>;
-    name: string;
-    decimals: number;
-  }>,
-  // There is no faucet on the mainnet
-  [Network.MAINNET]: [] as ReadonlyArray<{
-    symbol: TOKEN_SYMBOL.SUI;
-    type: string;
-    Icon: FC<SVGProps>;
-    name: string;
-    decimals: number;
-  }>,
-};
 
 export const FAUCET_TOKENS_V2 = {
   [Network.DEVNET]: [
@@ -201,7 +82,6 @@ export const FAUCET_TOKENS_V2 = {
     {
       symbol: TOKEN_SYMBOL.USDT,
       type: COIN_TYPE[Network.TESTNET].USDT,
-      Icon: USDTSVG,
       name: 'USDT',
       decimals: 9,
     },
